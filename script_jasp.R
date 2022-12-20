2fb29486f344a9ae3da1b9c0f79cd5996097a116
Henrik Singmann <singmann@gmail.com>
2022-12-20 16:47:46 +0100

    some more figures and fiddling

--- script_jasp.R
+++ script_jasp.R
@@ -25,7 +25,6 @@ fhch %>%
   mutate(ov_mean = mean(m)) %>% 
   mutate(diff = ov_mean - m)
 
-
 fhch_agg <- fhch %>% 
   group_by(id, task) %>% 
   summarise(rt = mean(rt))
@@ -134,7 +133,7 @@ lm2 <- lm(rt ~ task, fhch2)
 fhch2$resid_lm <- residuals(lm2)
 mm2 <- mixed(rt ~ task + (1|id), fhch2, method = "S")
 fhch2$resid_lmm <- residuals(mm2$full_model)
-ranef(mm2$full_model)
+
 
 p1 <- fhch2 %>% 
   group_by(id) %>% 
@@ -142,12 +141,12 @@ p1 <- fhch2 %>%
   filter(id %in% c("L1")) %>% 
   filter((rt < 0.8 & trial < 4) | (rt > 0.8 & trial < 40)) %>% 
   select(-trial) %>% 
-  slice(1:3, 6, 12)
+  slice(1:2, 6, 14, 15)
 p2 <- fhch2 %>% 
   group_by(id) %>% 
   mutate(trial = seq_len(n())) %>% 
   filter(id %in% c("L5")) %>% 
-  filter((rt < 1.3 & trial < 10) | (rt > 1.3 & trial < 5)) %>% 
+  slice(c(7, 35, 50, 49, 126)) %>% 
   select(-trial)
 
 out <- bind_cols(bind_rows(p0_a, tibble(id = NA), p0_b), 
@@ -155,6 +154,124 @@ out <- bind_cols(bind_rows(p0_a, tibble(id = NA), p0_b),
                  bind_rows(p1, tibble(id = NA), p2))
 
 print(xtable::xtable(out, digits = 1), include.rownames = FALSE)
+
+
+fhch_agg2 <- rownames_to_column(ranef(mm2$full_model)$id, var = "id") %>% 
+  rename(ri = "(Intercept)") %>% 
+  right_join(fhch_agg) %>% 
+  as_tibble()
+
+fhch_agg2 %>% 
+  select(-rt) %>% 
+  pivot_longer(cols = c(ri, resid)) %>% 
+  mutate(estimate = factor(name, levels = c("ri", "resid"), 
+                           labels = c("RI", "Observed"))) %>% 
+  ggplot(aes(x = value, y = estimate)) +
+  geom_point() +
+  geom_line(aes(group = id))
+
+
+##----------------------------------------------------------------
+##                        Include Stimulus                       -
+##----------------------------------------------------------------
+
+fhch %>% 
+  group_by(task, stimulus) %>% 
+  summarise(m = mean(rt)) %>% 
+  ungroup() %>% 
+  mutate(ov_mean = mean(m)) %>% 
+  mutate(diff = ov_mean - m)
+
+fhch %>% 
+  filter(stimulus == "nonword") %>% 
+  droplevels() %>% 
+  {levels(.$item)}
+
+fhch %>% 
+  filter(stimulus == "word") %>% 
+  droplevels() %>% 
+  {levels(.$item)}
+
+##
+
+
+fhch3 <- fhch %>% 
+  select(id, task, stimulus, item, rt)
+mm3 <- mixed(rt ~ task*stimulus + (stimulus|id), fhch3, method = "S")
+mm3
+mm4 <- mixed(rt ~ task*stimulus + (stimulus|id) + (task|item), fhch3, method = "S")
+mm4
+
+fhch3$resid_lmm1 <- residuals(mm3$full_model)
+fhch3$resid_lmm2 <- residuals(mm4$full_model)
+summary(mm3)
+summary(mm4)
+
+p1 <- fhch3 %>% 
+  group_by(id) %>% 
+  mutate(trial = seq_len(n())) %>% 
+  filter(id %in% c("L1")) %>% 
+  filter((rt < 0.8 & trial < 4) | (rt > 0.8 & trial < 40)) %>% 
+  select(-trial) %>% 
+  slice(1:2, 6, 14, 15)
+p2 <- fhch3 %>% 
+  group_by(id) %>% 
+  mutate(trial = seq_len(n())) %>% 
+  filter(id %in% c("L5")) %>% 
+  slice(c(7, 35, 50, 49, 126)) %>% 
+  select(-trial)
+
+# fhch3 %>% 
+#   group_by(id) %>% 
+#   mutate(trial = seq_len(n())) %>% 
+#   filter(id %in% c("L5")) %>% 
+#   View()
+
+out <- bind_rows(p1, tibble(id = NA), p2)
+
+print(xtable::xtable(out, digits = 1), include.rownames = FALSE)
+
+fhch3_eff <- fhch3 %>%
+  group_by(task, stimulus, id) %>% 
+  summarise(rt = mean(rt)) %>% 
+  group_by(id) %>% 
+  summarise(
+    mean = mean(rt),
+    effect = rt[1] - rt[2]
+  )
+
+cor.test(fhch3_eff$mean, fhch3_eff$effect)
+fhch3_eff %>% 
+  summarise(
+    cor(mean,effect)
+  )
+
+fhch3_eff2 <- left_join(
+  fhch3_eff, 
+  rownames_to_column(ranef(mm3$full_model)$id, var = "id")
+) %>% 
+  mutate(
+    inter_emp = mean - mean(mean),
+    effect_emp = effect - mean(effect)
+  ) %>% 
+  rename(
+    inter_est = "(Intercept)",
+    effect_est = stimulus1
+  ) %>% 
+  select(-mean, -effect)
+
+
+fhch3_eff2 %>% 
+  pivot_longer(-id, names_to = c("effect", "measure"), names_sep = "_") %>%
+  #pivot_wider(names_from = effect, values_from = value) %>% 
+  mutate(estimate = factor(measure, levels = c("est", "emp"), 
+                           labels = c("random", "observed"))) %>% 
+  ggplot(aes(x = value, y = estimate)) +
+  geom_point() +
+  geom_line(aes(group = id)) +
+  facet_wrap(vars(effect), scales = "free_x")
+
+
 
 ####
 
