1dcfe3569054b1d94d8066c5ae9901c6185bedc2
FBartos <f.bartos96@gmail.com>
2022-12-21 14:39:14 +0100

    Update script_jasp.R

--- script_jasp.R
+++ script_jasp.R
@@ -1,8 +1,8 @@
 
 library("tidyverse")
 library("afex")
-theme_set(theme_bw(base_size = 15) + 
-            theme(legend.position="bottom", 
+theme_set(theme_bw(base_size = 15) +
+            theme(legend.position="bottom",
                   panel.grid.major.x = element_blank()))
 
 options(pillar.sigfig = 2)
@@ -11,8 +11,8 @@ data("fhch2010")
 fhch <- as_tibble(droplevels(fhch2010[ fhch2010$correct,])) # remove errors
 str(fhch2010) # structure of the data
 
-fhch %>% 
-  group_by(id, stimulus) %>% 
+fhch %>%
+  group_by(id, stimulus) %>%
   count()
 
 ####
@@ -23,14 +23,14 @@ ylab <- "response time (s)"
 ##                        Aggregated Data                       -
 ##---------------------------------------------------------------
 
-fhch %>% 
-  group_by(task) %>% 
-  summarise(m = mean(rt)) %>% 
-  mutate(ov_mean = mean(m)) %>% 
+fhch %>%
+  group_by(task) %>%
+  summarise(m = mean(rt)) %>%
+  mutate(ov_mean = mean(m)) %>%
   mutate(diff = ov_mean - m)
 
-fhch_agg <- fhch %>% 
-  group_by(id, task) %>% 
+fhch_agg <- fhch %>%
+  group_by(id, task) %>%
   summarise(rt = mean(rt))
 lm1 <- lm(rt ~ task, fhch_agg)
 fhch_agg$resid <- residuals(lm1)
@@ -57,26 +57,26 @@ p1
 set.seed(1234566)  ## needed to ensure jittered points are at same position
 p1_de <- ggplot_build(p1)
 
-lap2 <- fhch_agg %>% 
+lap2 <- fhch_agg %>%
   mutate(xpoint = p1_de$data[[2]]$x)
 
-lap3 <- fhch_agg %>% 
-  group_by(task) %>% 
+lap3 <- fhch_agg %>%
+  group_by(task) %>%
   summarise(rt = mean(rt))
 
-lap4 <- lap3 %>% 
+lap4 <- lap3 %>%
   summarise(rt = mean(rt))
 
-lap5 <- p1_de$data[[1]] %>% 
-  group_by(x) %>% 
+lap5 <- p1_de$data[[1]] %>%
+  group_by(x) %>%
   summarise(xmin = first(xmin),
-            xmax = first(xmax)) %>% 
+            xmax = first(xmax)) %>%
   bind_cols(lap3)
 
-lap6 <- p1_de$data[[2]] %>% 
-  group_by(group) %>% 
-  mutate(rt = mean(y)) %>% 
-  as_tibble() %>% 
+lap6 <- p1_de$data[[2]] %>%
+  group_by(group) %>%
+  mutate(rt = mean(y)) %>%
+  as_tibble() %>%
   select(-size, - fill, -alpha, - stroke)
 
 lap7 <- cbind(lap3, mean_rt = lap4$rt, xxx = c(1.4, 1.6))
@@ -84,7 +84,7 @@ lap7 <- cbind(lap3, mean_rt = lap4$rt, xxx = c(1.4, 1.6))
 set.seed(1234566)
 p2 <- ggplot(fhch_agg, aes(x = task, y = rt)) +
   geom_violin(width = vio_width) +
-  geom_segment(data = lap6, 
+  geom_segment(data = lap6,
                mapping = aes(x = x, xend = x, y = rt, yend = y),
                colour = "grey") +
   ggbeeswarm::geom_quasirandom(width = point_width) +
@@ -100,7 +100,7 @@ p2 <- ggplot(fhch_agg, aes(x = task, y = rt)) +
 p2
 
 cowplot::plot_grid(p1, p2)
-ggsave("simple-residuals.pdf", device = "pdf", 
+ggsave("simple-residuals.pdf", device = "pdf",
        width = 19, height = 8, units = "cm")
 
 
@@ -108,11 +108,11 @@ ggsave("simple-residuals.pdf", device = "pdf",
 ##                        individual plots                       -
 ##----------------------------------------------------------------
 
-fhch_indiv <- fhch %>% 
-  filter(id %in% c("L1", "L5")) %>% 
+fhch_indiv <- fhch %>%
+  filter(id %in% c("L1", "L5")) %>%
   droplevels()
 
-fhch_indiv %>% 
+fhch_indiv %>%
   ggplot(aes(x = id, y = rt)) +
   geom_violin(width = vio_width) +
   ggbeeswarm::geom_quasirandom(width = point_width) +
@@ -128,15 +128,15 @@ fhch_indiv %>%
 
 
 
-fhch_agg %>% 
+fhch_agg %>%
   print(n = Inf)
 
-p0_a <- fhch_agg %>% 
+p0_a <- fhch_agg %>%
   filter(id %in% c(paste0("N", 1:7)))
-p0_b <- fhch_agg %>% 
+p0_b <- fhch_agg %>%
   filter(id %in% c(paste0("L", 1:6)))
 
-fhch2 <- fhch %>% 
+fhch2 <- fhch %>%
   select(id, task, rt)
 lm2 <- lm(rt ~ task, fhch2)
 fhch2$resid_lm <- residuals(lm2)
@@ -144,37 +144,37 @@ mm2 <- mixed(rt ~ task + (1|id), fhch2, method = "S")
 fhch2$resid_lmm <- residuals(mm2$full_model)
 
 
-p1 <- fhch2 %>% 
-  group_by(id) %>% 
-  mutate(trial = seq_len(n())) %>% 
-  filter(id %in% c("L1")) %>% 
-  filter((rt < 0.8 & trial < 4) | (rt > 0.8 & trial < 40)) %>% 
-  select(-trial) %>% 
+p1 <- fhch2 %>%
+  group_by(id) %>%
+  mutate(trial = seq_len(n())) %>%
+  filter(id %in% c("L1")) %>%
+  filter((rt < 0.8 & trial < 4) | (rt > 0.8 & trial < 40)) %>%
+  select(-trial) %>%
   slice(1:2, 6, 14, 15)
-p2 <- fhch2 %>% 
-  group_by(id) %>% 
-  mutate(trial = seq_len(n())) %>% 
-  filter(id %in% c("L5")) %>% 
-  slice(c(7, 35, 50, 49, 126)) %>% 
+p2 <- fhch2 %>%
+  group_by(id) %>%
+  mutate(trial = seq_len(n())) %>%
+  filter(id %in% c("L5")) %>%
+  slice(c(7, 35, 50, 49, 126)) %>%
   select(-trial)
 
-out <- bind_cols(bind_rows(p0_a, tibble(id = NA), p0_b), 
-                 NA, 
+out <- bind_cols(bind_rows(p0_a, tibble(id = NA), p0_b),
+                 NA,
                  bind_rows(p1, tibble(id = NA), p2))
 
 print(xtable::xtable(out, digits = 1), include.rownames = FALSE)
 
 
-fhch_agg2 <- rownames_to_column(ranef(mm2$full_model)$id, var = "id") %>% 
-  rename(ri = "(Intercept)") %>% 
-  right_join(fhch_agg) %>% 
+fhch_agg2 <- rownames_to_column(ranef(mm2$full_model)$id, var = "id") %>%
+  rename(ri = "(Intercept)") %>%
+  right_join(fhch_agg) %>%
   as_tibble()
 
-fhch_agg2 %>% 
-  select(-rt) %>% 
-  pivot_longer(cols = c(ri, resid)) %>% 
-  mutate(estimate = factor(name, levels = c("ri", "resid"), 
-                           labels = c("RI", "Observed"))) %>% 
+fhch_agg2 %>%
+  select(-rt) %>%
+  pivot_longer(cols = c(ri, resid)) %>%
+  mutate(estimate = factor(name, levels = c("ri", "resid"),
+                           labels = c("RI", "Observed"))) %>%
   ggplot(aes(x = value, y = estimate)) +
   geom_point() +
   geom_line(aes(group = id))
@@ -184,27 +184,27 @@ fhch_agg2 %>%
 ##                        Include Stimulus                       -
 ##----------------------------------------------------------------
 
-fhch %>% 
-  group_by(task, stimulus) %>% 
-  summarise(m = mean(rt)) %>% 
-  ungroup() %>% 
-  mutate(ov_mean = mean(m)) %>% 
+fhch %>%
+  group_by(task, stimulus) %>%
+  summarise(m = mean(rt)) %>%
+  ungroup() %>%
+  mutate(ov_mean = mean(m)) %>%
   mutate(diff = ov_mean - m)
 
-fhch %>% 
-  filter(stimulus == "nonword") %>% 
-  droplevels() %>% 
+fhch %>%
+  filter(stimulus == "nonword") %>%
+  droplevels() %>%
   {levels(.$item)}
 
-fhch %>% 
-  filter(stimulus == "word") %>% 
-  droplevels() %>% 
+fhch %>%
+  filter(stimulus == "word") %>%
+  droplevels() %>%
   {levels(.$item)}
 
 ##
 
 
-fhch3 <- fhch %>% 
+fhch3 <- fhch %>%
   select(id, task, stimulus, item, rt)
 mm3 <- mixed(rt ~ task*stimulus + (stimulus|id), fhch3, method = "S")
 mm3
@@ -216,24 +216,24 @@ fhch3$resid_lmm2 <- residuals(mm4$full_model)
 summary(mm3)
 summary(mm4)
 
-p1 <- fhch3 %>% 
-  group_by(id) %>% 
-  mutate(trial = seq_len(n())) %>% 
-  filter(id %in% c("L1")) %>% 
-  filter((rt < 0.8 & trial < 4) | (rt > 0.8 & trial < 40)) %>% 
-  select(-trial) %>% 
+p1 <- fhch3 %>%
+  group_by(id) %>%
+  mutate(trial = seq_len(n())) %>%
+  filter(id %in% c("L1")) %>%
+  filter((rt < 0.8 & trial < 4) | (rt > 0.8 & trial < 40)) %>%
+  select(-trial) %>%
   slice(1:2, 6, 14, 15)
-p2 <- fhch3 %>% 
-  group_by(id) %>% 
-  mutate(trial = seq_len(n())) %>% 
-  filter(id %in% c("L5")) %>% 
-  slice(c(7, 35, 50, 49, 126)) %>% 
+p2 <- fhch3 %>%
+  group_by(id) %>%
+  mutate(trial = seq_len(n())) %>%
+  filter(id %in% c("L5")) %>%
+  slice(c(7, 35, 50, 49, 126)) %>%
   select(-trial)
 
-# fhch3 %>% 
-#   group_by(id) %>% 
-#   mutate(trial = seq_len(n())) %>% 
-#   filter(id %in% c("L5")) %>% 
+# fhch3 %>%
+#   group_by(id) %>%
+#   mutate(trial = seq_len(n())) %>%
+#   filter(id %in% c("L5")) %>%
 #   View()
 
 out <- bind_rows(p1, tibble(id = NA), p2)
@@ -241,46 +241,69 @@ out <- bind_rows(p1, tibble(id = NA), p2)
 print(xtable::xtable(out, digits = 1), include.rownames = FALSE)
 
 fhch3_eff <- fhch3 %>%
-  group_by(task, stimulus, id) %>% 
-  summarise(rt = mean(rt)) %>% 
-  group_by(id) %>% 
+  group_by(task, stimulus, id) %>%
+  summarise(rt = mean(rt)) %>%
+  group_by(id) %>%
   summarise(
     mean = mean(rt),
     effect = rt[1] - rt[2]
   )
 
 cor.test(fhch3_eff$mean, fhch3_eff$effect)
-fhch3_eff %>% 
+fhch3_eff %>%
   summarise(
     cor(mean,effect)
   )
 
 fhch3_eff2 <- left_join(
-  fhch3_eff, 
+  fhch3_eff,
   rownames_to_column(ranef(mm3$full_model)$id, var = "id")
-) %>% 
+) %>%
   mutate(
     inter_emp = mean - mean(mean),
     effect_emp = effect - mean(effect)
-  ) %>% 
+  ) %>%
   rename(
     inter_est = "(Intercept)",
     effect_est = stimulus1
-  ) %>% 
+  ) %>%
   select(-mean, -effect)
 
 
-fhch3_eff2 %>% 
+fhch3_eff2 %>%
   pivot_longer(-id, names_to = c("effect", "measure"), names_sep = "_") %>%
-  #pivot_wider(names_from = effect, values_from = value) %>% 
-  mutate(estimate = factor(measure, levels = c("est", "emp"), 
-                           labels = c("random", "observed"))) %>% 
+  #pivot_wider(names_from = effect, values_from = value) %>%
+  mutate(estimate = factor(measure, levels = c("est", "emp"),
+                           labels = c("random", "observed"))) %>%
   ggplot(aes(x = value, y = estimate)) +
   geom_point() +
   geom_line(aes(group = id)) +
   facet_wrap(vars(effect), scales = "free_x")
 
 
+### F-table --
+print_f <- function(f, df1, df2){
+  sprintf("F(%2$s, %3$.1f) = %1$.2f", f, df1, df2)
+}
+
+table_terms  <- c("task", "stimulus", "task:stimulus")
+table_models <- list(mm2, mm3, mm4)
+
+F_table <- do.call(cbind, lapply(table_models, function(m){
+  col <- NULL
+  for(term in table_terms){
+    if(term %in% row.names(m$anova_table)){
+      col <- c(col, print_f(m$anova_table[term, "F"], m$anova_table[term, "num Df"], m$anova_table[term, "den Df"]))
+    }else{
+      col <- c(col, "")
+    }
+  }
+  return(data.frame(m = col))
+}))
+rownames(F_table) <- table_terms
+xtable::xtable(F_table)
+
+mm3$anova_table["task", "F"]
 
 ####
 
